00956512b6d7c1537ad94150cc2df57f4acbfe2d
Homer <homermoncallo@gmail.com>
2020-06-15 02:47:12 -0430

    Primer Commit

---
+++ server/controllers/viajesController.js
@@ -0,0 +1,17 @@
+const Viaje = require('../models/Viajes')
+const Testimonial = require('../models/Testimoniales')
+
+exports.mostrarViajes =  async (req, res) => {
+    const viajes = await Viaje.findAll()
+        res.render('viajes', {
+            pagina: 'Próximos Viajes',
+            viajes
+        })
+}
+
+exports.mostrarViaje = async (req, res) => {
+    const viaje = await Viaje.findByPk(req.params.id)
+        res.render('viaje', {
+            viaje
+        })
+}
